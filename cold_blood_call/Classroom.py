3d69fdf31e2ef0c94badc3a56a2bf9812729e132
Mert <mertyapucu2000@gmail.com>
2022-01-12 00:29:13 -0800

    Classroom commented

--- cold_blood_call/Classroom.py
+++ cold_blood_call/Classroom.py
@@ -1,32 +1,69 @@
 import random
 
 class Classroom():
-    def __init__(self,roster, deckSize):
+    """Classroom data structure class. Contains 3 lists that organize the cold
+    call order of the classroom, and functions that manipulate these lists"""
+
+    def __init__(self, roster, deckSize):
+        """Takes in:
+        roster: a list of Student objects(only names rn), for the class roster
+        deckSize: integer, to decide the size of deck.
+
+        Initializes an empty postDeck, and calls the createDeck() method from
+        itself to create the deck. The remaining students stay in preDeck."""
+
         self.roster = roster
         self.preDeck = roster
         self.postDeck = []
         self.deckSize = deckSize
         self.deck = self.createDeck()
 
+
     def createDeck(self):
+        """Uses random library to choose random students from preDeck and
+        creates a temporary list for the deck.
+
+        Returns: list[Student], deck that will
+        be initialized as the self.deck"""
+
         gonnaBeDeck = []
         for i in range(self.deckSize):
             chosen = random.randint(0,len(self.preDeck)-1)
             gonnaBeDeck.append(self.preDeck[chosen])
             self.preDeck.remove(self.preDeck[chosen])
-        print("Deck Created")
+        # print("Deck Created")
         print(gonnaBeDeck, self.preDeck)
         return gonnaBeDeck
 
 
     def moveToDeck(self):
+        """Called by: moveToPost() from the same class
+
+        Uses random to choose a student from preDeck and moves it to deck.
+        If the preDeck is empty, calls the refresh method from the class to
+        re-occupy it."""
+
         if (len(self.preDeck) == 0):
             self.refresh()
         nextIndex = random.randint(0,len(self.preDeck)-1)
         self.deck.append(self.preDeck[nextIndex])
         self.preDeck.remove(self.preDeck[nextIndex])
 
-    def moveToPost(self,index): #may change the name to something better
+
+    def moveToPost(self,index,flag=0):
+        """Takes in:
+        index: integer, to know which student is chosen and will be moved to
+        postDeck
+        flag: boolean, will be true if the instructor set the flag.
+
+        Called by: InstructorInterface, to communicate user choice and request
+        new deck data.
+
+        Moves the student from deck to postDeck, and calls the moveToDeck() from
+        the class to fill the spot
+
+        Returns: list[Student], which will be used by InstructorInterface"""
+
         self.postDeck.append(self.deck[index])
         self.deck.remove(self.deck[index])
         self.moveToDeck()
@@ -34,10 +71,18 @@ class Classroom():
         print(self.deck,self.preDeck,self.postDeck)
         return self.deck
 
+
     def getDeck(self):
+        """Returns the current deck: list[Student]."""
         return self.deck
 
+
     def refresh(self):
+        """Called by: moveToDeck from the same class
+        
+        When a student is needed from preDeck but it is empty, this function is
+        called. Moves every student in postDeck to preDeck."""
+
         for student in self.postDeck:
             self.preDeck.append(student)
             self.postDeck.remove(student)
--- cold_blood_call/InstructorInterface.py
+++ cold_blood_call/InstructorInterface.py
@@ -25,54 +25,16 @@ class InstructorInterface():
             y+=10
             self.nameLabels.append(label)
 
-
-        # label1 = Label(self.root, text=f"{self.roster[0]}")
-        # label1.pack(padx=5, pady=15, side=tk.LEFT)
-        #
-        # label2 = Label(self.root, text="asd2")
-        # label2.pack(padx=5, pady=20, side=tk.LEFT)
-        #
-        # label3 = Label(self.root, text="asd2")
-        # label3.pack(padx=5, pady=25, side=tk.LEFT)
-
         button = Button(self.root, text="click", command=self.buttonHit)
         button.pack(padx=5, pady=30, side=tk.LEFT)
 
-        # self.nameLabels.append(button)
-
-        # for index,name in enumerate(self.roster):
-        #     self.nameLabels.append(Label(self.root, text=f"{name}").grid(row=0,column=index))
-        #     print(self.nameLabels[index])
-
-        # self.button = Button(self.root, text="click", command=self.buttonHit).grid(row=0, column=4)
-        # for index,name in enumerate(self.roster):
-        #     self.nameLabels.append(tk.Label(self.frame, text=f"{name}").grid(column=index, row=0))
-        #
-        # self.btn = tk.Button(self.frame, text="asd", command=self.buttonHit ).grid(column=5, row=0)
 
 
     def startGUI(self):
-        # self.root.after(1000, self.checkUpdate)
         self.root.mainloop()
 
     def kill(self):
-        self.running = 0
-        # self.addRandom()
-        # self.root.destroy()
-
-    def chooseStudent(self):
-        print("set")
-        self.index = 1
-
-    def checkUpdate(self):
-        print("s")
-        # while (1):
-
-        if(self.index != -1):
-            self.roster = self.callback(self.index)
-            self.index =-1
-        self.root.update_idletasks()
-        self.root.after(1000,self.checkUpdate)
+        self.root.destroy()
 
     def buttonHit(self):
 
