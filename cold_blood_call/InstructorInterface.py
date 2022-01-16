0c64e638e5d9c53ee1df1845d7314cbb0d00d6ed
sleveckis <sleveckis@gmail.com>
2022-01-15 16:10:42 -0800

    GUI push

--- cold_blood_call/InstructorInterface.py
+++ cold_blood_call/InstructorInterface.py
@@ -1,63 +1,151 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import filedialog
-import random
-from threading import Thread
-import time
+"""
+    Created by Stephen Leveckis, 1/12/2022
+    Creates self.window GUI for cold calling program
 
+    Installs Requried:
+        Tkinter: sudo apt-get install python3.6-tk
+"""
+import tkinter as tk
+from tkinter import *
 
 class InstructorInterface():
     def __init__(self, given, callback):
-        self.root = Tk()
-        if not given:
-            self.rosterFile = filedialog.askopenfilename(initialdir="", title="Please choose your roster file")
-            return
+        self.win = tk.Tk()
+        # All text starts as white by default
+        self.text_colors = ["white", "white", "white", "white"]
 
-        # self.frame = Frame(self.root, padding=50)
-        # self.frame.grid()
-        self.root.grid()
-        self.root.geometry("+1000+0")
-        self.roster = given
-        self.index = 0
-        self.callback = callback
-        self.deckSize = 4
-        self.nameLabels = []
+        # Leftmost value is True (highlighted) by default
+        self.highlight_list = [True, False, False, False]
 
-        y = 15
-        for i in range(self.deckSize):
-            if (i==self.index):
-                label = Label(self.root,text=f"{self.roster[i]}",bg = "red")
+        self.highlight_counter = 0
+
+        # Key listeners as part of the Tkinter library, waits for key press
+        self.win.bind('<Right>', self.rightArrowKey)
+        self.win.bind('<Left>', self.leftArrowKey)
+
+        # Gets native screen resolution width and height
+        screen_w = self.win.winfo_screenwidth()
+        screen_h = self.win.winfo_screenheight()
+
+        # 19 is a scalar modifier that happens to create a decent
+        # screen height for our self.win based on original native screen height
+        self.win_h = screen_h/22
+        self.win_w = screen_w 
+
+        # Make a string "widthxheight" to pass to geometry function
+        dimensions = "%dx%d" % (self.win_w, self.win_h)
+        # Sets the self.window size to these dimensions
+        self.win.geometry(dimensions)
+
+        # Instantiate name variables to read in from the "deck" data structure
+        self.name1 = "Thomas Python"
+        self.name2 = "Johnny Hammersticks"
+        self.name3 = "Susan Walkway"
+        self.name4 = "Theodore Crumpet"
+
+        # Canvas object
+        self.canvas = Canvas(self.win, width = self.win_w, height = self.win_h, bg = "black")
+
+        """
+            Create 4 widgets, one for each displayed name.
+
+            This process, creating 4 widgets must be done once initially, here,
+            and then once for every keypress to display the updated text
+            (done in the arrow key functions)
+        """
+        for i in range(len(self.highlight_list)):
+            if (self.highlight_list[i] is True):
+                self.text_colors[i] = "red"
+
+        self.canvas.create_text(5,15, text=self.name1, fill = self.text_colors[0], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(self.win_w/4, 15, text=self.name2, fill = self.text_colors[1], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(self.win_w/2, 15, text=self.name3, fill = self.text_colors[2], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(((self.win_w*3) /4), 15, text=self.name4, fill = self.text_colors[3], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.pack(fill=BOTH, expand=True)
+    
+
+    """
+        Deletes all old text objects and replaces them with updated ones based on the 
+        text_colors list.
+    """
+    def displayText(self):
+        self.canvas.delete("all")
+        self.canvas.create_text(5,15, text=self.name1, fill = self.text_colors[0], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(self.win_w/4, 15, text=self.name2, fill = self.text_colors[1], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(self.win_w/2, 15, text=self.name3, fill = self.text_colors[2], font = ('Helvetica 15 bold'), anchor='w')
+        self.canvas.create_text(((self.win_w*3) /4), 15, text=self.name4, fill = self.text_colors[3], font = ('Helvetica 15 bold'), anchor='w')
+
+    """
+    Increases highlight_counter with a bound that prevents it from
+    increasing past 3.
+    """
+    def increaseCounter(self):
+        #global highlight_counter
+        if ((self.highlight_counter +1) > 3):
+            self.highlight_counter = 3
+        else:
+            self.highlight_counter = self.highlight_counter + 1
+
+    """
+    Decreases highlight_counter with a bound that prevents it from
+    decreasing past zero.
+    """
+    def decreaseCounter(self):
+        #global highlight_counter
+        if((self.highlight_counter -1) < 0):
+            self.highlight_counter = 0 
+        else:
+            self.highlight_counter = self.highlight_counter - 1
+
+
+    """
+
+    """
+    def leftArrowKey(event, self):
+        #global highlight_counter
+        # Set the boolean list to reflect which index
+        # in the list we want to be highlighted 
+        self.decreaseCounter()
+        self.highlight_list[self.highlight_counter+1] = False
+        self.highlight_list[self.highlight_counter] = True
+
+        for i in range(len(self.highlight_list)):
+            if (self.highlight_list[i] is True):
+                self.text_colors[i] = "red"
             else:
-                label = Label(self.root,text=f"{self.roster[i]}",bg = "green")
-            label.pack(padx=5, pady=y, side=tk.LEFT)
-            y+=10
-            self.nameLabels.append(label)
+                self.text_colors[i] = "white"
 
-        button = Button(self.root, text="click", command=self.buttonHit)
-        button.pack(padx=5, pady=30, side=tk.LEFT)
+        displayText()    
 
+    """
+        Current index starts at 0, the furthest left displayed name.
+        Right arrow key press should highlight a name to the right of the current index.
+        Appropriately, modify the highlight_list to be false at the current index, and
+        True one index to the right.
 
+        Also set the text_colors list in the same manner.
+
+        Finally, create text objects to display the desired behavior:
+        the initial index going from red to white, and the index to the right
+        going from white to red
+
+    """
+    def rightArrowKey(event, self):
+        #global highlight_counter
+        self.increaseCounter()
+
+        self.highlight_list[self.highlight_counter-1] = False
+        self.highlight_list[self.highlight_counter] = True
+
+        for i in range(len(self.highlight_list)):
+            if (self.highlight_list[i] is True):
+                self.text_colors[i] = "red"
+            else:
+                self.text_colors[i] = "white"
+
+        displayText()
+
+    # Let it rip
     def startGUI(self):
-        self.root.wm_attributes("-topmost", "true")
-        self.root.lift()
-        self.root.mainloop()
+        self.win.mainloop()
 
-    def kill(self):
-        self.root.destroy()
-
-    def updateGUI(self):
-        """Will be called by keybind callback functions to update the GUI after
-        the changes. It stops the mainloop and takes care of the waiting functions.
-         It recolorizes the labels and updates the name with the new deck"""
-        self.root.update_idletasks()
-        for index,label in enumerate(self.nameLabels):
-            if (index == self.index):
-                label.config(text=f"{self.roster[index]}", bg="red")
-            else:
-                label.config(text=f"{self.roster[index]}", bg="green")
-            label.update()
-
-        time.sleep(0.1)
-
-    def getRosterFileInput(self):
-        return self.rosterFile
